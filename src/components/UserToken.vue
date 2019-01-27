b7e4f7eab7d088a98e54406b8e3d3a12d5202c05
ryosms <ryo.sms@gmail.com>
2019-01-27 23:03:23 +0900

    Mod Home
    remove default component.
    add a component for input user token.

---
+++ src/components/UserToken.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="md-layout md-gutter md-alignment-center-left">
+    <md-field class="md-layout-item">
+      <label>Username</label>
+      <md-input v-model="username"></md-input>
+    </md-field>
+    <md-field class="md-layout-item">
+      <label>Token</label>
+      <md-input v-model="token" type="password"></md-input>
+    </md-field>
+    <md-button class="md-layout-item md-raised" @click="authentication">Submit</md-button>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import {MdField, MdButton} from 'vue-material/dist/components';
+
+  Vue.use(MdField);
+  Vue.use(MdButton);
+
+  export default {
+    name: 'UserToken',
+    computed: {
+      username: {
+        get() {
+          return this.$store.state.username;
+        },
+        set(value) {
+          this.$store.commit('setUsername', value);
+        },
+      },
+      token: {
+        get() {
+          return this.$store.state.token;
+        },
+        set(value) {
+          this.$store.commit('setToken', value);
+        },
+      },
+    },
+    methods: {
+      authentication() {
+        alert(this.username);
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .md-layout-item {
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <UserToken/>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+  import {Component, Vue} from 'vue-property-decorator';
+  import UserToken from '@/components/UserToken.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      UserToken,
+    },
+  })
+  export default class Home extends Vue {
+  }
 </script>
