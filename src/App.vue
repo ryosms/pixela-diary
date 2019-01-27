7ac6800f791917e424f96d0c25c9f6207129819f
ryosms <ryo.sms@gmail.com>
2019-01-27 22:52:58 +0900

    Mod App root
    mod title, load web font, and Modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page-container">
+      <md-app md-mode="fixed">
+        <md-app-toolbar class="md-primary">
+          <span class="md-title"><router-link to="/">Pixela Diary</router-link></span>
+          <div class="md-toolbar-section-end">
+            <router-link to="/about">About</router-link>
+          </div>
+        </md-app-toolbar>
+        <md-app-content>
+          <router-view/>
+        </md-app-content>
+      </md-app>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+  import Vue from 'vue';
+  import {MdApp, MdToolbar, MdContent, MdButton} from 'vue-material/dist/components';
+
+  Vue.use(MdApp);
+  Vue.use(MdToolbar);
+  Vue.use(MdContent);
+  Vue.use(MdButton);
+
+  import 'vue-material/dist/theme/default.css';
+  import 'vue-material/dist/vue-material.min.css';
+
+  export default {
+    name: 'App',
+  };
+</script>
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  .md-app-toolbar {
+    a {
+      color: white !important;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
-}
 </style>
