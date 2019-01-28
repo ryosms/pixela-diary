13af24e8a3481a64bc911e34bb2fc186136aa239
ryosms <ryo.sms@gmail.com>
2019-01-28 23:30:27 +0900

    Add loading bar

--- src/components/UserToken.vue
+++ src/components/UserToken.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <md-progress-bar md-mode="indeterminate" v-if="isLoading"></md-progress-bar>
     <div class="md-layout md-gutter md-alignment-center-left">
       <md-field class="md-layout-item">
         <label>Username</label>
@@ -11,10 +12,9 @@
       </md-field>
       <md-button class="md-layout-item md-raised" @click="authentication">Submit</md-button>
     </div>
-
-    <md-snackbar md-position="center" :md-duration="Infinity" :md-active.sync="showError">
+    <md-snackbar md-position="center" :md-duration="Infinity" :md-active.sync="hasError">
       <span class="md-accent">Something went wrong...</span>
-      <md-button class="md-accent" @click="showError = false">OK</md-button>
+      <md-button class="md-accent" @click="hasError = false">OK</md-button>
     </md-snackbar>
 
   </div>
@@ -22,17 +22,19 @@
 
 <script>
   import Vue from 'vue';
-  import {MdField, MdButton, MdSnackbar} from 'vue-material/dist/components';
+  import {MdField, MdButton, MdSnackbar, MdProgress} from 'vue-material/dist/components';
   import axios from 'axios';
 
   Vue.use(MdField);
   Vue.use(MdButton);
   Vue.use(MdSnackbar);
+  Vue.use(MdProgress);
 
   export default {
     name: 'UserToken',
     data: () => ({
-      showError: false,
+      hasError: false,
+      isLoading: false,
     }),
     computed: {
       username: {
@@ -54,14 +56,17 @@
     },
     methods: {
       async authentication() {
+        this.isLoading = true;
         try {
           const res = await axios.get('https://pixe.la/v1/users/' + this.$store.state.username + '/graphs',
             {headers: {'X-USER-TOKEN': this.$store.state.token}});
           this.$store.commit('setGraphs', res.data.graphs);
-          this.showError = false;
+          this.hasError = false;
           this.$router.push('/graphs');
         } catch (e) {
-          this.showError = true;
+          this.hasError = true;
+        } finally {
+          this.isLoading = false;
         }
       },
     },
