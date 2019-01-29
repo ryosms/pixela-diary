abb5aa18c6dd20943041a5de6161e4b446681fad
Ryo Sumasu <ryo.sms@gmail.com>
2019-01-29 18:00:48 +0900

    Impl displaying list of graphs.

--- src/views/GraphList.vue
+++ src/views/GraphList.vue
@@ -1,21 +1,26 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="graph in this.$store.state.graphs">
-        {{ graph.name }}
-      </li>
-    </ul>
-  </div>
-  
+  <md-list>
+    <GraphListItem v-for="graph in this.$store.state.graphs"
+                   :key="graph.id" v-bind:graph="graph"/>
+  </md-list>
 </template>
 
 <script>
+  import Vue from 'vue';
+  import {MdList} from 'vue-material/dist/components';
+  import GraphListItem from '@/components/GraphListItem.vue';
+
+  Vue.use(MdList);
+
   export default {
     name: 'GraphList',
     created() {
       if (this.$store.state.graphs.length === 0) {
         this.$router.replace('/');
       }
+    },
+    components: {
+      GraphListItem,
     },
   };
 </script>
