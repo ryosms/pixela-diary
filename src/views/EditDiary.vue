569e26f79f8aff4b27b067ff35ae9f9f25cc1bfb
ryosms <ryo.sms@gmail.com>
2019-01-30 00:24:43 +0900

    Add DatePicker to EditDiary view

--- src/views/EditDiary.vue
+++ src/views/EditDiary.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>{{ $route.params.graph_id }}</div>
+  <div v-if="graph">
+    <span class="md-title">{{ graph.name }}</span>
+    <md-datepicker v-model="diaryDate" md-immediately>
+      <label>diary date</label>
+    </md-datepicker>
+  </div>
 </template>
 
 <script>
+  import Vue from 'vue';
+  import {MdDatepicker, MdDialog} from 'vue-material/dist/components';
+
+  Vue.use(MdDatepicker);
+  Vue.use(MdDialog);
+
   export default {
     name: 'EditDiary',
-    created() {
+    data: () => ({
+      graph: null,
+      diaryDate: null,
+    }),
+    async created() {
       if (!this.$store.state.username || !this.$store.state.token) {
         this.$router.replace('/');
       }
+      this.diaryDate = new Date(Date.now());
+      for (let i in this.$store.state.graphs) {
+        let g = this.$store.state.graphs[i];
+        if(g.id === this.$route.params.graph_id) {
+          this.graph = g;
+          return;
+        }
+      }
+      this.$router.replace('/graphs')
     },
   };
 </script>
