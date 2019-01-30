c4587c70c3cef414565b17d59b0fa5ded19feabc
ryosms <ryo.sms@gmail.com>
2019-01-30 23:36:34 +0900

    Impl load registered diary

--- src/components/DiaryDetail.vue
+++ src/components/DiaryDetail.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    <span>{{ diaryDate.getFullYear() }}年</span>
-    <span>{{ diaryDate.getMonth() + 1 }}月</span>
-    <span>{{ diaryDate.getDate() }}日</span>
-    <div>{{graphId}}</div>
-  </div>
+  <md-card>
+    <md-card-content class="md-layout">
+      <md-field class="md-layout-item md-size-50 md-small-size-100">
+        <label>Title</label>
+        <md-input v-model="title"></md-input>
+      </md-field>
+      <md-field class="md-layout-item md-size-100">
+        <label>Body</label>
+        <md-textarea v-model="body" md-autogrow></md-textarea>
+      </md-field>
+    </md-card-content>
+    <md-card-actions>
+      <md-button class="md-primary" :disabled="!canEdit">Save</md-button>
+    </md-card-actions>
+  </md-card>
 </template>
 
 <script>
+  import Vue from 'vue';
+  import {MdCard, MdField, MdButton, MdSnackbar, MdProgress} from 'vue-material/dist/components';
+  import axios from 'axios';
+
+  Vue.use(MdCard);
+  Vue.use(MdField);
+
   export default {
     name: 'DiaryDetail',
     props: {
@@ -16,11 +32,22 @@
       graphId: String,
       token: String,
     },
+    computed: {
+      canEdit: {
+        get() {
+          return !this.hasError;
+        },
+        set(ignore) {
+        },
+      },
+    },
+    data: () => ({
+      title: 'title',
+      body: 'body',
+      hasError: false,
+    }),
     methods: {
       isDateChanged(newDate, oldDate) {
-        if (!newDate) {
-          return false;
-        }
         if (!oldDate) {
           return true;
         }
@@ -33,14 +60,47 @@
           + ('0' + diaryDate.getMonth() + 1).slice(-2)
           + ('0' + diaryDate.getDate()).slice(-2);
       },
-      async loadDiary() {
+      loadDiary() {
+        const self = this;
         const url = 'https://pixe.la/v1/users/' + this.username
           + '/graphs/' + this.graphId + '/' + this.formatDiaryDate(this.diaryDate);
-        console.log(url);
+        const headers = {
+          'X-USER-TOKEN': this.token,
+          'accept': 'application/json'
+        };
+        axios.get(url, {headers: headers})
+          .then(function (response) {
+            self.hasError = false;
+            self.parseOptionalData(response.data.optionalData);
+          })
+          .catch(error => {
+            // FIXME: in case of 404, pixela don't return CORS headers.
+            // self.hasError = (!error.response || error.response.status !== 404);
+            self.title = '';
+            self.body = '';
+          });
       },
+      parseOptionalData(optionalData) {
+        try {
+          const json = JSON.parse(optionalData);
+          this.title = json.title;
+          this.body = json.body;
+        } catch (ignore) {
+          this.title = '';
+          this.body = '';
+        }
+      },
+    },
+    created() {
+      this.loadDiary();
     },
     watch: {
       diaryDate(newDate, oldDate) {
+        if (!newDate) {
+          this.canEdit = false;
+          return;
+        }
+        this.canEdit = true;
         if (!this.isDateChanged(newDate, oldDate)) {
           return;
         }
